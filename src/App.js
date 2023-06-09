315257b1481075f54b5bd4294f19b4ff96e35417
Nandani Gajjar <nandani.gajjar.23@gmail.com>
2023-06-09 19:22:48 +0530

    registration form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Registration from "./Components/registration";
+function App() {
+  return (
+    <div className="App">
+      <Registration />
+    </div>
+  );
+}
+
+export default App;
